1a3f3800bb87eb31f445546f5ccbc0bf6815e101
Edward Xiao <edkyxiao@gmail.com>
2025-08-03 15:37:38 +1000

    Update js.js

--- public/js.js
+++ public/js.js
@@ -174,4 +174,30 @@ document.addEventListener("DOMContentLoaded", () => {
             window.location.href = "video.html";
         });
     }
+
+    // Stage
+    const image = document.getElementById('mode-image');
+    const button = document.getElementById('toggle-image-mode');  // <button>
+    const buttonImage = document.getElementById('button-icon');   // <img> inside button
+
+    let isLight = true;
+
+    button.addEventListener('click', function() {
+        // Fade out the image
+        image.style.opacity = 0;
+
+        setTimeout(() => {
+            // Change main image
+            image.src = isLight ? 'images/stage_dark.png' : 'images/stage_light.png';
+
+            // Change button icon image
+            buttonImage.src = isLight ? 'images/white_sun_icon.png' : 'images/black_sun_icon.png';
+
+            // Fade back in
+            image.style.opacity = 1;
+
+            // Toggle state
+            isLight = !isLight;
+        }, 300);
+    });
 });
